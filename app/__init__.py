0ff9695ad0d90c83ce8ecc5cb865229dffca1758
olizzz <xinchen.zhao.20@dartmouth.edu>
2019-05-16 15:22:02 -0400

    added all

--- app/__init__.py
+++ app/__init__.py
@@ -26,5 +26,20 @@ def create_app(test_config=None):
     @app.route('/')
     def hello():
         return 'Hello, World!'
+    
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
+    @app.route('/')
+    def hello():
+        return 'Hello, World!'
 
     return app
+
